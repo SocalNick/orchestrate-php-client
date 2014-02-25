60e228fdcc7c8a4662f7315858dd669ac130ad2e
Nicholas Calugar <njcalugar@gmail.com>
2014-02-24 21:06:06 -0800

    Testing successful GET

--- tests/CollectionTest.php
+++ tests/CollectionTest.php
@@ -21,6 +21,19 @@ class CollectionTest extends \PHPUnit_Framework_TestCase
         $httpClient->shouldReceive('get')
             ->with('first_collection/missing_key', array(), array('auth' => array('api-key')))
             ->andReturn($missingRequest);
+
+        $godfatherResponse = m::mock('Guzzle\Http\Message\Response');
+        $godfatherResponse->shouldReceive('json')
+            ->withNoArgs()
+            ->andReturn(json_decode('{"Title": "The Godfather","Released": "24 Mar 1972"}', true));
+        $godfatherRequest = m::mock('Guzzle\Http\Message\Request');
+        $godfatherRequest->shouldReceive('send')
+            ->withNoArgs()
+            ->andReturn($godfatherResponse);
+        $httpClient->shouldReceive('get')
+            ->with('films/the_godfather', array(), array('auth' => array('api-key')))
+            ->andReturn($godfatherRequest);
+
         $this->client = new Client('api-key', $httpClient);
     }
 
@@ -34,5 +47,16 @@ class CollectionTest extends \PHPUnit_Framework_TestCase
         $kvFetchOp = new KvFetchOperation("first_collection", "missing_key");
         $result = $this->client->execute($kvFetchOp);
         $this->assertNull($result);
+    }
+
+    public function testGet()
+    {
+        $kvFetchOp = new KvFetchOperation("films", "the_godfather");
+        $result = $this->client->execute($kvFetchOp);
+        $this->assertInternalType('array', $result);
+        $this->assertArrayHasKey('Title', $result);
+        $this->assertArrayHasKey('Released', $result);
+        $this->assertEquals('The Godfather', $result['Title']);
+        $this->assertEquals('24 Mar 1972', $result['Released']);
     }
 }
